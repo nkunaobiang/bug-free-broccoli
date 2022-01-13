b063232f024bd71107da09de82ad8a5d9d2695fa
Foo bar <nkunaobiang@gmail.com>
2022-01-13 22:18:41 +0100

    devoir de programmation python

--- polynomial.py
+++ polynomial.py
@@ -1,36 +1,98 @@
 from cmath import sqrt
 import matplotlib.pyplot as plt  # type:ignore
+import polynomial as P
+import numpy as np
 
 
 class Poly2:
     """ Classe permettant de representer un polynôme de degré 2."""
 
-    def __init__(self, *coeffs):
+    def __init__(self, a,b,c):
         """ Méthode constructeur qui prend en paramètre, les coefficients du polynôme"""
-        pass
+        """ a,b,c etant la coefficient du polynome"""
+        self.a=a
+        self.b=b
+        self.c=c
 
-    def __add__(self, other):
+
+    def __add__(self, a,b,m,n):
         """Addition 2 polynômes et qui renvoi du nouveau polynôme"""
-        pass
 
-    def __sub__(self, other):
+        """ soit a[],b[], representent respectivement la liste des polynomes a et b"""
+        """ m,n representent respectivement la taille des ta bleaux a et b"""
+
+        taille = max(m,n)
+        sum = [0 for i in range(taille)]
+        # Initialise du polynome produit
+     
+        for i in range(0, m, 1):
+            sum[i] = a[i]
+
+         # prendre les termes du premier polynom
+        for i in range(n):
+            sum[i] += b[i]
+
+        return sum
+
+        
+
+
+    def __sub__(self, m,n,a,b):
         """Soustraction de 2 polynômes et renvoi du nouveau polynôme"""
-        pass
 
+        """ soit a[],b[], representent respectivement la liste des coefficients des polynomes a et b"""
+        """ m,n representent respectivement la taille des ta bleaux a et b"""
+
+        taille = max(m,n)
+        subs = [0 for i in range(taille)]
+        # Initialise du polynome produit
+     
+        for i in range(0, m, 1):
+            subs[i] = a[i]
+
+         # prendre les termes du premier polynom
+        for i in range(n):
+            subs[i] -= b[i]
+
+        return subs
+        
+
+
+    """On pouvait le faire autrement"""
     def __repr__(self):
         msg = 'Poly2(' + ', '.join([str(c) for c in sorted(self.coeffs.values())]) + ')'
         return msg
 
-    def __str__(self):
+
+
+    def __str__(self,poly,n):
         """Méthode qui personalise la chaîne de caractère affichée par la fonction print
         Si: p1 = Poly(3, -4, 2)
         Alors print(p1) affiche: '2X^2 - 4X + 3'
         """
-        pass
+        for i in range(n):
+            print(poly[i], end = "")
+            if (i != 0):
+                print("x^", i, end = "")
+            if (i != n - 1):
+                print(" + ", end = "")
+        
 
-    def solve(self):
+
+    def solve(self,disc,a,b,c):
         """ Méthode qui renvoie les solutions si elles existent."""
-        pass
+
+        disc = sqrt(b*b-4*a*c)
+
+        if disc >=0:
+            x1 = (-b+disc)/(2*a)
+            x2 = (-b-disc)/(2*a)
+            print("les racines sont:", x1, x2)
+
+        else:
+            print("Pas de racines")
+
+    
 
     def __val(self, x):
         """ Méthode qui calcule et renvoie la valeur de y en fonction de x.
@@ -38,11 +100,28 @@ class Poly2:
         Si: x prend pour valeur 5
         Alors: y = 5^2 + 1 = 26
         """
-        pass
+        y=int(sqrt(x)+1)
+
+        return y
+
 
     def draw(self, x_points=None):
         """ Méthode qui trace la courbe, voir fichier png."""
-        pass
+
+        x = np.linspace(-2,2,100)
+        y = np.exp(x)
+        plt.title('Courbe d\' une fonction exponentielle')
+        plt.xlabel('x')
+        plt.ylabel('f(x)')
+        plt.plot(x, y, alpha = 0.4,
+        color ='yellow', linestyle ='dashed',
+        linewidth = 2)
+        plt.grid(alpha =.6, linestyle ='-')
+        plt.plot(x,y, 'y', label='f(x)=e^x')
+        #Placer la lagénde
+        plt.legend(loc='upper left')
+        plt.show()
+        
 
 
 if __name__ == "__main__":
